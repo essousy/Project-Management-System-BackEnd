bd34f674256ce37394fe7f1404e650fb583a9b15
unknown <imadimad200103@gmail.com>
2026-05-13 21:38:17 +0100

    Add Jwt Configuration

---
+++ src/main/java/com/pms/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.pms.config;
+
+public class JwtConstant {
+
+    public static final String SECRETE_KEY="fjkezsnb hfzejufezhrfuezhurfuzej bfsdhjfbsdbfdshfbhsdfhsdbh";
+    public static final String JWT_HEADER="Authorization";
+
+}
