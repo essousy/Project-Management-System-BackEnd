e382cbf7176ede1f2aefcf6adad614671d0a9f38
unknown <imadimad200103@gmail.com>
2026-05-14 23:44:09 +0100

    Add JwtProvider

--- src/main/java/com/pms/config/JwtProvider.java
+++ src/main/java/com/pms/config/JwtProvider.java
@@ -1,8 +1,34 @@
 package com.pms.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.crypto.SecretKey;
+import java.util.Collection;
+import java.util.Date;
+
 public class JwtProvider {
 
-    
+    static SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRETE_KEY.getBytes());
+//    Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
+//
+//    String email = String.valueOf(claims.get("email"));
+//    String authorities = String.valueOf(claims.get("authorities"));
+
+    public static String generateToken(Authentication auth) {
+
+        String jwt = Jwts.builder().setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime()+86400000))
+                .claim("email", auth.getName()).signWith(key).compact();
+
+        return jwt;
+    }
+
+
+
 
 
 }
