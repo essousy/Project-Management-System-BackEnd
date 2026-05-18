76cc51ca5efa126eedcb08850e66020eef6c80fc
unknown <imadimad200103@gmail.com>
2026-05-18 23:58:19 +0100

    Implement User Service

--- src/main/java/com/pms/service/UserService.java
+++ src/main/java/com/pms/service/UserService.java
@@ -1,4 +1,12 @@
 package com.pms.service;
 
+import com.pms.model.User;
+
 public interface UserService {
+    User findUserProfileByJwt(String jwt) throws Exception;
+    User findUserByEmail(String email) throws Exception;
+    User findUserById(Long userid) throws Exception;
+    User updateUsersProjectSize(User user, int number);
+
+
 }
