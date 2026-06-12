7d1f0a6a247ce59fec5f13237d6471ab97423fdb
essousy <imadimad200103@gmail.com>
2026-06-13 00:01:42 +0100

    Add Project Api

---
+++ src/main/java/com/pms/controller/ProjectController.java
@@ -0,0 +1,99 @@
+package com.pms.controller;
+
+import com.pms.model.Chat;
+import com.pms.model.Project;
+import com.pms.model.User;
+import com.pms.response.MessageResponse;
+import com.pms.service.ProjectService;
+import com.pms.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/projects")
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<Project>> getProjects(
+            @RequestParam(required = false)String category,
+            @RequestParam(required = false)String tag,
+            @RequestHeader("Authorization")String jwt
+    ) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        List<Project> projects = projectService.getProjectByTeam(user,category,tag);
+        return new ResponseEntity<>(projects, HttpStatus.OK);
+    }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<Project> getProjectById(
+            @PathVariable Long projectId,
+            @RequestHeader("Authorization")String jwt
+    ) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        Project projects = projectService.getProjectById(projectId);
+        return new ResponseEntity<>(projects, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Project> createProject(
+            @RequestHeader("Authorization")String jwt,
+            @RequestBody Project project
+    ) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        Project createdProject = projectService.createProject(project,user);
+        return new ResponseEntity<>(createdProject, HttpStatus.OK);
+    }
+
+    @PatchMapping("/{projectId}")
+    public ResponseEntity<Project> udpateProject(
+            @PathVariable Long projectId,
+            @RequestHeader("Authorization")String jwt,
+            @RequestBody Project project
+    ) throws Exception {
+        Project updatedProject = projectService.updateProject(project,projectId);
+        return new ResponseEntity<>(updatedProject, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<MessageResponse> deleteProject(
+            @PathVariable Long projectId,
+            @RequestHeader("Authorization")String jwt
+    ) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+         projectService.deleteProject(projectId,user.getId());
+         MessageResponse res = new MessageResponse("project deleted successfully");
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Project>> searchProjects(
+            @RequestParam(required = false)String keyword,
+            @RequestHeader("Authorization")String jwt
+    ) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        List<Project> projects = projectService.searchProjects(keyword,user);
+        return new ResponseEntity<>(projects, HttpStatus.OK);
+    }
+
+    @GetMapping("/{projectId}/chat")
+    public ResponseEntity<Chat> getChatByProjectId(
+            @PathVariable Long projectId,
+            @RequestHeader("Authorization")String jwt
+    ) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        Chat chat = projectService.getChatByProjectId(projectId);
+        return new ResponseEntity<>(chat, HttpStatus.OK);
+    }
+
+
+}
