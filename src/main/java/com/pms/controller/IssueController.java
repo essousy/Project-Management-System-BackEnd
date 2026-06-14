6dd747c62e0a98417c279f217da1bf92bd154ec4
essousy <imadimad200103@gmail.com>
2026-06-14 23:33:44 +0100

    Add Issue Controller

---
+++ src/main/java/com/pms/controller/IssueController.java
@@ -0,0 +1,38 @@
+package com.pms.controller;
+
+import com.pms.model.Issue;
+import com.pms.service.IssueService;
+import com.pms.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/issues")
+public class IssueController {
+
+    @Autowired
+    private IssueService issueService;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping("/{issueId}")
+    public ResponseEntity<Issue> getIssueById(@PathVariable Long issueId) throws Exception{
+
+        return ResponseEntity.ok(issueService.getIssueById(issueId));
+    }
+
+    @GetMapping("/project/{projectId}")
+    public ResponseEntity<List<Issue>> getIssueByProjectId(@PathVariable Long projectId) throws Exception{
+        return ResponseEntity.ok(issueService.getIssueByProjectId(projectId));
+    }
+
+
+}
